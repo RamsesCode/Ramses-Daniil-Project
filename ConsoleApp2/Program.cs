78202eba7d02fecad432e9531135a90a0af7927f
Ramses Sanchez <ramsessanchez25@gmail.com>
2025-02-13 14:31:20 -0500

    Class added

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,22 +1,34 @@
-﻿// These are changes only for Ramses Branch
+﻿using System;
+
+// These are changes only for Ramses Branch
 
 
 
 //Class to go here 
 
 
+public class Appointment
+
 // Constructor to begin here
+{
+private bool isMinuteFree(int period, int minute)
+    {
+        
+        return true; 
+    }
+
+private void reserveBlock(int period, int startMinute, int duration)
+    {
+        //This will be to reserve a space, we are keeping in the private portion of the constructor 
+        Console.WriteLine($"Block reserved from minute {startMinute} for {duration} minutes in period {period}");
+    }
+
+    }
 
 
 
 //Functions will take place here 
 
-
-
-
-
-
-//Function to Search dates that are empty 
 
 
 
@@ -37,5 +49,8 @@
 
 
 
+    
+    
 
-Console.WriteLine("Hello Ramses and Daniil");
+
+    
