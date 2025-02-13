3c6a2c6f92da2672c95a696320f7294723df13c4
Ramses Sanchez <ramsessanchez25@gmail.com>
2025-02-13 14:02:44 -0500

    Ramses Changes

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,41 @@
-﻿
+﻿// These are changes only for Ramses Branch
+
+
+
+//Class to go here 
+
+
+// Constructor to begin here
+
+
+
+//Functions will take place here 
+
+
+
+
+
+
+//Function to Search dates that are empty 
+
+
+
+
+
+//Function for reserving a block 
+
+
+
+
+//Function for finding free blocks 
+
+
+
+
+//to make appointment 
+
+
+
+
 
 Console.WriteLine("Hello Ramses and Daniil");
