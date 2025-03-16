92a2dc78b6c2f1215e895966847cc116c623692f
daniilnsdk <daniilnsdk@gmail.com>
2025-03-15 20:56:12 -0400

    Implemented findFreeBlock and makeAppointment methods

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,78 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
 
-Console.WriteLine("Hello Ramses and Daniil");
+// These are changes only for Daniil Branch
+
+public class Appointment
+{
+    // Array for the list of periods that are available
+    private List<int>[] reservedMinutes = new List<int>[8];
+
+    // Constructor to initialize the schedule with random free durations
+    public Appointment()
+    {
+        Random rand = new Random();
+        for (int i = 0; i < 8; i++)
+        {
+            reservedMinutes[i] = new List<int>();
+            int freeDuration = rand.Next(0, 31); // Randomly choosing free duration between 0 and 30 minutes
+            for (int j = 0; j < freeDuration; j++)
+            {
+                reservedMinutes[i].Add(j);
+            }
+        }
+    }
+
+    // This will begin the portion where we look if any free minutes in a period 
+    private bool isMinuteFree(int period, int minute)
+    {
+        return !reservedMinutes[period - 1].Contains(minute);
+    }
+
+    // This will be to reserve a space, we are keeping in the private portion of the constructor 
+    private void reserveBlock(int period, int startMinute, int duration)
+    {
+        for (int i = startMinute; i < startMinute + duration; i++)
+        {
+            reservedMinutes[period - 1].Add(i);
+        }
+        Console.WriteLine($"Block reserved from minute {startMinute} for {duration} minutes in period {period}");
+    }
+
+    // Function for finding free blocks
+    public int findFreeBlock(int period, int duration)
+    {
+        for (int startMinute = 0; startMinute <= 60 - duration; startMinute++)
+        {
+            bool blockAvailable = true;
+            for (int j = 0; j < duration; j++)
+            {
+                if (!isMinuteFree(period, startMinute + j))
+                {
+                    blockAvailable = false;
+                    break;
+                }
+            }
+            if (blockAvailable)
+            {
+                return startMinute;
+            }
+        }
+        return -1; // No available block found
+    }
+
+    // Function to make an appointment
+    public bool makeAppointment(int startPeriod, int endPeriod, int duration)
+    {
+        for (int period = startPeriod; period <= endPeriod; period++)
+        {
+            int startMinute = findFreeBlock(period, duration);
+            if (startMinute != -1)
+            {
+                reserveBlock(period, startMinute, duration);
+                return true;
+            }
+        }
+        return false; // No available time slot found in the given period range
+    }
+}
--- ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48a1855346db5c6fea4d66e699c136448ed67cc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68be583558e5949ee460aef61101870b3d4a1e98")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
